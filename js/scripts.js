b8d17c4545e08178bbeeaa5d8e74d47f1e8b47ae
adeniran adejuwon <adeniran adejuwon>
2023-11-21 05:01:31 -0800

    add multiplication

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// function to add
+// function modulus(number1,number2){
+//   return number1 % number2 ;
+// }
+
+// modulus function
+// function modulus(number1,number2){
+//   return number1 % number2 ;
+// }
+
+// 
+// 
+function multiplication(number1,number2){
+  return number1 *number2 ;
+}
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+
+// alert(modulus(number1, number2))
+//alert(add(number1, number2))
+alert(multiplication(number1, number2))
